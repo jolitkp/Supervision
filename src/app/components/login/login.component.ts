6f9359309ab84f2708950724e42c8be987cc10ff
jolitkp <tokpanoujoliana@gmail.com>
2023-06-08 11:19:07 +0100

    Angular

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,15 +15,18 @@ export class LoginComponent {
     password: null
   };
 
+  public error = null;
+
   constructor( private http: HttpClient){}
 
   onSubmit() {
-     return this.http.post('http://127.0.0.1:8000/api/login', this.form).subscribe(
+     return this.http.post('http://127.0.0.1:8000/api/login-page', this.form).subscribe(
       data => console.log(data),
       error => console.log(error)
      );
   }
-
-  handleError() {
-  }
+  
+  /*handleError(error) {
+    this.error = error.error.message;
+  }*/
 }
