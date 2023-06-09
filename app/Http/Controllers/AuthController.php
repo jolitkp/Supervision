72d454f9000ffefed96682d0d763b6c3f47116d8
jolitkp <tokpanoujoliana@gmail.com>
2023-06-09 12:01:17 +0100

    laravel version 2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class AuthController extends Controller
@@ -12,7 +14,7 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
 
-        if (! $token = Auth::guard('api')->attempt($credentials)) {
+        if (!$token = Auth::guard('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
@@ -20,7 +22,7 @@ class AuthController extends Controller
     }*/
 
     
-    public function login(Request $request)
+      public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
@@ -36,5 +38,66 @@ class AuthController extends Controller
                 'error' => 'Email and password doesn\'t exist',
             ], 401);
         }}
+
+
+        /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    /*protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()->name
+        ]);
+    }*/
+
+
+    
+   /* public function signup(Request $request){
         
+        User::create($request->all());
+        return $this->login($request);
+    }*/
+
+    public function signup(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required',
+            'prenom'=> 'required',
+            'email'=> 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'password_confirm' => 'required|same:password',
+            'role'=>'required'
+        
+            
+        ]);
+
+        $user = new User;
+        $user->nom = $request->nom;
+        $user->prenom = $request->prenom;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = $request->role;
+        $user->save();
+
+        return response()->json([
+            'message' => 'Utilisateur enregistré avec succès',
+            'user' => $user
+        ]);
+    }
 }
+
+
+
+
+
+
+
+        
+
